65a0ce1aab2f763182479f2a89bd7fcfb3aec30b
EmreSimseek <emre0816s@gmail.com>
2024-10-01 22:47:44 +0300

    Organize directory structure

---
+++ Turkcell-CV/basics/301/changin_colorspace.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+
+path = "E:/Opencv/OpenCV-101/TCell01/images/"
+
+# RGB görüntü
+img = cv.imread(path +"logo.png")
+cv.namedWindow("rgb", cv.WINDOW_AUTOSIZE)
+cv.imshow("rgb",img)
+cv.waitKey(1)
+
+# RGB to GRAY
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow("gray",gray)
+cv.waitKey(1)
+
+# RGB to HSV
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+cv.imshow("hsv",hsv)
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+
