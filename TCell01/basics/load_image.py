083ba4afc6e40d434e9c1fbab8e8f7ca86159cba
EmreSimseek <emre0816s@gmail.com>
2024-10-01 11:13:45 +0300

    Update load_image.py

--- TCell01/basics/load_image.py
+++ TCell01/basics/load_image.py
@@ -1,14 +1,14 @@
 import cv2 as cv
 
 path = "E:/Opencv/OpenCV-101/TCell01/images/"
-img = cv.imread(path + "zoo.jpg")
+img = cv.imread(path + "zoo.jpg")   #1 Resim dosya adresi verildi
 
 print(type(img))
 if img is None:
     print(f"Resim dosyası yüklenemedi: {path}")
 else:
-    cv.namedWindow("opencv_test", cv.WINDOW_AUTOSIZE)
-    cv.imshow('opencv_test', img)
+    cv.namedWindow("opencv_test", cv.WINDOW_AUTOSIZE) #Pencere oluşturuldu
+    cv.imshow('opencv_test', img) #Gösterildi ve bir alt satırda süresi belirlendi
     cv.waitKey(0)  # Sonsuz bekleme, pencereyi kapatmak için bir tuşa basılmalı
 
 cv.destroyAllWindows()
