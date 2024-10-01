585b68ed5ffbfa63de2bc4df779ffd64baf2997f
EmreSimseek <emre0816s@gmail.com>
2024-10-01 15:00:14 +0300

    Update pixel_value_statistics.py

--- TCell01/basics/pixel_value_statistics.py
+++ TCell01/basics/pixel_value_statistics.py
@@ -3,19 +3,34 @@ import  numpy as np
 
 path = "E:/Opencv/OpenCV-101/TCell01/images/"
 
+#opencv logo
 img = cv.imread(path + "logo.png")
 cv.imshow("logo", img)
 cv.waitKey(1)
 
 
 #cvtColor
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow("gray", img)
-cv.waitKey(0)
+gray_opencv = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imwrite(path + "gray_opencv.png", gray_opencv) #gray_opencv.png gri resim dosyası olarak klasöre eklendi
 
-cv.imwrite(path + "gray_opencv.png", gray) #gray_opencv.png gri resim dosyası olarak klasöre eklendi
-
-
+#gray_opencv oluşturuldu
 cv.namedWindow("gray",cv.WINDOW_AUTOSIZE)
-cv.imshow("gray", gray_opencv.png)
+cv.imshow("gray", gray_opencv)
+cv.waitKey(1)
+
+#minMaxLoc
+
+min_value, max_value, min_loc, max_loc = cv.minMaxLoc(gray_opencv)
+print("min value: %.2f, max value: %.2f" %(min_value,max_value))
+
+print(f"min loc:{min_loc}, max loc:", max_loc)
+
+#meanStdDev
+means, stddev = cv.meanStdDev(gray_opencv)
+print("mean: %.2f, stddev: %.2f" % (means,stddev))
+
+gray_opencv[np.where(gray_opencv < means)] = 0
+gray_opencv[np.where(gray_opencv > means)] = 255
+
+cv.imshow("white_and_black", gray_opencv)
 cv.waitKey(0)
