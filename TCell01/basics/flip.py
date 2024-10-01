0030f32f6ca21b675ece8e30b8d83aafd250509b
EmreSimseek <emre0816s@gmail.com>
2024-10-01 15:07:38 +0300

    Create flip.py

---
+++ TCell01/basics/flip.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+
+path = "E:/Opencv/OpenCV-101/TCell01/images/"
+img = cv.imread(path + "logo.png")
+
+# Original
+cv.imshow("Original", img)
+cv.waitKey(1)
+# X flip
+dst = cv.flip(img,1)
+cv.imshow("X-flip", dst)
+cv.waitKey(1)
+
+# Y flip
+dst = cv.flip(img,0)
+cv.imshow("Y-flip", dst)
+cv.waitKey(1)
+
+# X-Y flip
+dst = cv.flip(img,-1)
+cv.imshow("XY-flip", dst)
+cv.waitKey(0)
