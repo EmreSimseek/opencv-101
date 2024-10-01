9d0e549651eac4ae646d127c3292c352992e65c8
EmreSimseek <emre0816s@gmail.com>
2024-10-01 10:15:41 +0300

    Renamed

--- TCell01/basics/gray_image.py
+++ TCell01/basics/gray_image.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-path = "E:/Opencv/TCell01/"
+path = "E:/Opencv/OpenCV-101/TCell01/images/"
 
 img = cv.imread(path + "zoo.jpg")
 #img = cv.imread(path + "opencv.png", cv.IMREAD_GRAYSCALE) direkt olarak gri skala göstermek
--- TCell01/basics/load_image.py
+++ TCell01/basics/load_image.py
@@ -1,12 +1,14 @@
 import cv2 as cv
 
-path = "E:/Opencv/TCell01/"
+path = "E:/Opencv/OpenCV-101/TCell01/images/"
 img = cv.imread(path + "zoo.jpg")
 
-
+print(type(img))
 if img is None:
     print(f"Resim dosyası yüklenemedi: {path}")
 else:
     cv.namedWindow("opencv_test", cv.WINDOW_AUTOSIZE)
     cv.imshow('opencv_test', img)
     cv.waitKey(0)  # Sonsuz bekleme, pencereyi kapatmak için bir tuşa basılmalı
+
+cv.destroyAllWindows()
